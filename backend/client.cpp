12a3c447bfe172873cc85cd7c1482d6cbe125ad3
Braden Watling <braden.watling@mail.utoronto.ca>
2017-01-01 14:21:52 -0500

    Added very basic boost asio client and server with makefile

---
+++ backend/client.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    boost::asio::io_service io_service;
+
+    tcp::resolver resolver(io_service);
+    tcp::endpoint endpoint(tcp::v4(), 9999);
+
+    tcp::socket socket(io_service);
+    boost::system::error_code error = boost::asio::error::host_not_found;
+    while (error)
+    {
+      socket.close();
+      socket.connect(endpoint, error);
+    }
+    if (error)
+      throw boost::system::system_error(error);
+
+    for (;;)
+    {
+      boost::array<char, 128> buf;
+      boost::system::error_code error;
+
+      size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+      if (error == boost::asio::error::eof)
+        break; // Connection closed cleanly by peer.
+      else if (error)
+        throw boost::system::system_error(error); // Some other error.
+
+      std::cout.write(buf.data(), len);
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
+
