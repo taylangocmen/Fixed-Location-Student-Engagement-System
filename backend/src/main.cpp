1b4e42fb6947e6c13bb806591cc009a4b132109f
Braden Watling <braden.watling@mail.utoronto.ca>
2017-01-07 22:30:22 -0500

    Removed all asio stuff and replaced it with POCO

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,19 +1,79 @@
-#include "server.h"
+#include <Poco/Net/HTTPServer.h>
+#include <Poco/Net/HTTPRequestHandler.h>
+#include <Poco/Net/HTTPServerParams.h>
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/Util/ServerApplication.h>
+#include <Poco/DateTime.h>
+#include <Poco/DateTimeFormatter.h>
+#include <Poco/DateTimeFormat.h>
+#include <Poco/Net/SecureServerSocket.h>
+#include <iostream>
 
-int main()
+class Handler : public Poco::Net::HTTPRequestHandler
 {
-  try
+public:
+  void handleRequest(Poco::Net::HTTPServerRequest &request, Poco::Net::HTTPServerResponse &response)
   {
-    boost::asio::io_service io_service;
-    Server s(io_service, 9999);
-    s.start();
-    io_service.run();
+    response.setChunkedTransferEncoding(false);
+    response.setContentType("text/html");
+
+    Poco::DateTime now;
+    std::string timeString(Poco::DateTimeFormatter::format(now, Poco::DateTimeFormat::SORTABLE_FORMAT));
+
+    std::ostream& responseStream = response.send();
+    responseStream << "<html><head><head><title>Simple HTTP Server</title></head>";
+    responseStream << "<body><h1>Current Time</h1><p>";
+    responseStream << ( timeString );
+    responseStream << "</p></body></html>";
+    std::cout << "Received request" << std::endl;
   }
-  catch (std::exception& e)
+};
+
+class HandlerFactory : public Poco::Net::HTTPRequestHandlerFactory
+{
+public:
+  Poco::Net::HTTPRequestHandler *createRequestHandler(const Poco::Net::HTTPServerRequest &request)
   {
-    std::cerr << e.what() << std::endl;
+    return new Handler();
+  }
+};
+
+class Server : public Poco::Util::ServerApplication
+{
+public:
+  Server()
+  {
+    Poco::Net::initializeSSL();
   }
 
-  return 0;
-}
+  ~Server()
+  {
+    Poco::Net::uninitializeSSL();
+  }
+
+protected:
+  void initialize(Application& self)
+  {
+    loadConfiguration(); // load default configuration files, if present
+    ServerApplication::initialize(self);
+  }
+
+  int main(const std::vector<std::string> &args)
+  {
+    Poco::Net::SecureServerSocket socket(9999);
+    Poco::Net::HTTPServerParams *pParams = new Poco::Net::HTTPServerParams();
+    pParams->setMaxQueued(100);
+    pParams->setMaxThreads(16);
+    Poco::Net::HTTPServer server(new HandlerFactory(), socket, pParams);
+    server.start();
+    std::cout << "Server started!" << std::endl;
+    waitForTerminationRequest();
+    std::cout << "Stopping server!" << std::endl;
+    server.stop();
+    return EXIT_OK;
+  }
+};
+
+POCO_SERVER_MAIN(Server);
 
