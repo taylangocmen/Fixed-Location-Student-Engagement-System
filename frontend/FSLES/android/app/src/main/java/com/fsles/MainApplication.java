c4e92ad6439900d42977d23e09ce7b463eb91383
Taylan Gocmen <ta.gocmen@gmail.com>
2017-01-10 21:44:08 -0500

    Restarted the mobile app

--- frontend/FSLES/android/app/src/main/java/com/fsles/MainApplication.java
+++ frontend/FSLES/android/app/src/main/java/com/fsles/MainApplication.java
@@ -1,4 +1,4 @@
-package com.mobile;
+package com.fsles;
 
 import android.app.Application;
 import android.util.Log;
--- frontend/FSLES/ios/FSLESTests/FSLESTests.m
+++ frontend/FSLES/ios/FSLESTests/FSLESTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface mobileTests : XCTestCase
+@interface FSLESTests : XCTestCase
 
 @end
 
-@implementation mobileTests
+@implementation FSLESTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
