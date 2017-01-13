e0a6d597299aa616934ffa3252b024f2a29e76e9
EC2 Default User <ec2-user@ip-172-31-16-127.us-west-2.compute.internal>
2017-01-13 20:08:15 +0000

    Fix schema

--- backend/schema_questions.sql
+++ backend/schema_questions.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS ece496.questions (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   class_id INTEGER,
-  weight INTEGER, --optional question weight
-  title VARCHAR(64), -- short title for question.
-  num_qs INTEGER NOT NULL, --the number of answers (for multiple choice) 2-8
+  weight INTEGER, #optional question weight
+  title VARCHAR(64), # short title for question.
+  num_qs INTEGER NOT NULL, #the number of answers (for multiple choice) 2-8
   question VARCHAR(255),
-  ans_1 VARCHAR(255), -- for text based answers, use ans_1 to hold the correct answer
+  ans_1 VARCHAR(255), # for text based answers, use ans_1 to hold the correct answer
   ans_2 VARCHAR(255),
   ans_3 VARCHAR(255),
   ans_4 VARCHAR(255),
@@ -13,10 +13,10 @@ CREATE TABLE IF NOT EXISTS ece496.questions (
   ans_6 VARCHAR(255),
   ans_7 VARCHAR(255),
   ans_8 VARCHAR(255),
-  correct_ans_mc INTEGER, -- the integer value of the correct answer (2-8) in the case of multiple choice
+  correct_ans_mc INTEGER, # the integer value of the correct answer (2-8) in the case of multiple choice
   time_created TIMESTAMP,
-  time_opened TIMESTAMP, -- soonest time when the prof opened the question
-  time_closed -- time when the prof closed the question
+  time_opened TIMESTAMP, # soonest time when the prof opened the question
+  time_closed TIMESTAMP, # time when the prof closed the question
   FOREIGN KEY(class_id) REFERENCES ece496.class(id)
 );
 
