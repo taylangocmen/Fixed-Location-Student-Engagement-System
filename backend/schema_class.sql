747d80dd76b930099783ab82c1a8899af1a353f6
EC2 Default User <ec2-user@ip-172-31-16-127.us-west-2.compute.internal>
2017-01-13 20:08:15 +0000

    Fix schema

--- backend/schema_class.sql
+++ backend/schema_class.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS ece496.class (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT, -- id of the section
-  prof_id INTEGER NOT NULL, -- prof id
-  sizeof INTEGER NOT NULL, -- number of students in the class
+  id INTEGER PRIMARY KEY AUTO_INCREMENT, # id of the section
+  prof_id INTEGER NOT NULL, # prof id
+  sizeof INTEGER NOT NULL, # number of students in the class
   time_created TIMESTAMP, 
-  latitude FLOAT, -- location of the class
+  latitude FLOAT, # location of the class
   longitude FLOAT,
-  time_loc TIMESTAMP, --time when the location was last updated
+  time_loc TIMESTAMP, #time when the location was last updated
   radius FLOAT, 
   FOREIGN KEY(prof_id) REFERENCES ece496.users(id)
 );
