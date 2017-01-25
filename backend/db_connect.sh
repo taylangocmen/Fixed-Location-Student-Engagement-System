b0da23e74d3c96316e493a129ae189eee46dca82
Braden Watling <braden.watling@mail.utoronto.ca>
2017-01-25 19:59:06 +0000

    Fix db connect script

--- backend/db_connect.sh
+++ backend/db_connect.sh
@@ -6,13 +6,13 @@ PASSWORD=password
 
 MYSQL="mysql --host=$HOST --user=$USER --password=$PASSWORD"
 
-if [ $1 = "create" ]; then
+if [ "$1" == "create" ] ; then
   echo 'Creating users table' && cat schema_users.sql | $MYSQL
   echo 'Creating courses table' && cat schema_courses.sql | $MYSQL
   echo 'Creating users_courses table' && cat schema_users_courses.sql | $MYSQL
   echo 'Creating questions table' && cat schema_questions.sql | $MYSQL
   echo 'Creating submissions table' && cat schema_submissions.sql | $MYSQL
-elif [ $1 = "destroy" ]; then
+elif [ "$1" == "destroy" ]; then
   echo 'drop table ece496.submissions' | $MYSQL
   echo 'drop table ece496.questions' | $MYSQL
   echo 'drop table ece496.users_courses' | $MYSQL
