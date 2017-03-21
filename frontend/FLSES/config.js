d25c9b794dac2590274e1b00627b0eda3ff824d0
Taylan Gocmen <ta.gocmen@gmail.com>
2017-03-21 18:32:14 -0400

    dirty hack, have a better idea?

--- frontend/FLSES/config.js
+++ frontend/FLSES/config.js
@@ -6,8 +6,7 @@ export const config = {
   apiBaseUrl: 'http://ec2-54-213-241-203.us-west-2.compute.amazonaws.com',
   // TODO: remove this test user
   testUser: {
-    username: "blah",
-    pass_hash: "1234",
-    token: '29f47c51cff22333914c11122e5f8a3547bbe5adffb43f8192e079cb5b9e6a2f'
+    username: 'utoridnu',
+    pass_hash: 'password',
   }
 };
