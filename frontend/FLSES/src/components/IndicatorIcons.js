bfe79e54673f4d85cc248728d1e92f0a4b0ffa73
Taylan Gocmen <ta.gocmen@gmail.com>
2017-04-02 22:43:42 -0400

    working v1

--- frontend/FLSES/src/components/IndicatorIcons.js
+++ frontend/FLSES/src/components/IndicatorIcons.js
@@ -5,111 +5,111 @@ import * as colors from '../styling/Colors';
 
 
 export const CourseGrey = (style) => <Image
-  source={require('../material-design-icons/action/ios/ic_account_balance_48pt.imageset/ic_account_balance_48pt.png')}
+  source={require('../material-design-icons/ic_account_balance_48pt/ic_account_balance_48pt.png')}
   style={style}
 />;
 
 export const CourseWhite = (style) => <Image
-  source={require('../material-design-icons/action/ios/ic_account_balance_white_48pt.imageset/ic_account_balance_white_48pt.png')}
+  source={require('../material-design-icons/ic_account_balance_white_48pt/ic_account_balance_white_48pt.png')}
   style={style}
 />;
 
 
 export const ChartGrey = (style) => <Image
-  source={require('../material-design-icons/action/ios/ic_assessment_48pt.imageset/ic_assessment_48pt.png')}
+  source={require('../material-design-icons/ic_assessment_48pt/ic_assessment_48pt.png')}
   style={style}
 />;
 
 export const ChartWhite = (style) => <Image
-  source={require('../material-design-icons/action/ios/ic_assessment_white_48pt.imageset/ic_assessment_white_48pt.png')}
+  source={require('../material-design-icons/ic_assessment_white_48pt/ic_assessment_white_48pt.png')}
   style={style}
 />;
 
 
 export const QuestionGrey = (style) => <Image
-  source={require('../material-design-icons/action/ios/ic_assignment_48pt.imageset/ic_assignment_48pt.png')}
+  source={require('../material-design-icons/ic_assignment_48pt/ic_assignment_48pt.png')}
   style={style}
 />;
 
 export const QuestionWhite = (style) => <Image
-  source={require('../material-design-icons/action/ios/ic_assignment_white_48pt.imageset/ic_assignment_white_48pt.png')}
+  source={require('../material-design-icons/ic_assignment_white_48pt/ic_assignment_white_48pt.png')}
   style={style}
 />;
 
 
 export const TickGrey = (style) => <Image
-  source={require('../material-design-icons/action/ios/ic_done_48pt.imageset/ic_done_48pt.png')}
+  source={require('../material-design-icons/ic_done_48pt/ic_done_48pt.png')}
   style={style}
 />;
 
 export const TickWhite = (style) => <Image
-  source={require('../material-design-icons/action/ios/ic_done_white_48pt.imageset/ic_done_white_48pt.png')}
+  source={require('../material-design-icons/ic_done_white_48pt/ic_done_white_48pt.png')}
   style={style}
 />;
 
 
 export const DoubleTickGrey = (style) => <Image
-  source={require('../material-design-icons/action/ios/ic_done_all_48pt.imageset/ic_done_all_48pt.png')}
+  source={require('../material-design-icons/ic_done_all_48pt/ic_done_all_48pt.png')}
   style={style}
 />;
 
 export const DoubleTickWhite = (style) => <Image
-  source={require('../material-design-icons/action/ios/ic_done_all_white_48pt.imageset/ic_done_all_white_48pt.png')}
+  source={require('../material-design-icons/ic_done_all_white_48pt/ic_done_all_white_48pt.png')}
   style={style}
 />;
 
 
 export const StarGrey = (style) => <Image
-  source={require('../material-design-icons/action/ios/ic_grade_48pt.imageset/ic_grade_48pt.png')}
+  source={require('../material-design-icons/ic_grade_48pt/ic_grade_48pt.png')}
   style={style}
 />;
 
 export const StarWhite = (style) => <Image
-  source={require('../material-design-icons/action/ios/ic_grade_white_48pt.imageset/ic_grade_white_48pt.png')}
+  source={require('../material-design-icons/ic_grade_white_48pt/ic_grade_white_48pt.png')}
   style={style}
 />;
 
 
 export const MinusGrey = (style) => <Image
-  source={require('../material-design-icons/content/ios/ic_remove_circle_outline_48pt.imageset/ic_remove_circle_outline_48pt.png')}
+  source={require('../material-design-icons/ic_remove_circle_outline_48pt/ic_remove_circle_outline_48pt.png')}
   style={style}
 />;
 
 export const MinusWhite = (style) => <Image
-  source={require('../material-design-icons/content/ios/ic_remove_circle_outline_white_48pt.imageset/ic_remove_circle_outline_white_48pt.png')}
+  source={require('../material-design-icons/ic_remove_circle_outline_white_48pt/ic_remove_circle_outline_white_48pt.png')}
   style={style}
 />;
 
 
 export const PlusGrey = (style) => <Image
-  source={require('../material-design-icons/content/ios/ic_add_circle_outline_48pt.imageset/ic_add_circle_outline_48pt.png')}
+  source={require('../material-design-icons/ic_add_circle_outline_48pt/ic_add_circle_outline_48pt.png')}
   style={style}
 />;
 
 export const PlusWhite = (style) => <Image
-  source={require('../material-design-icons/content/ios/ic_add_circle_outline_white_48pt.imageset/ic_add_circle_outline_white_48pt.png')}
+  source={require('../material-design-icons/ic_add_circle_outline_white_48pt/ic_add_circle_outline_white_48pt.png')}
   style={style}
 />;
 
 
 export const BtGoodGrey = (style) => <Image
-  source={require('../material-design-icons/device/ios/ic_bluetooth_connected_48pt.imageset/ic_bluetooth_connected_48pt.png')}
+  source={require('../material-design-icons/ic_bluetooth_connected_48pt/ic_bluetooth_connected_48pt.png')}
   style={style}
 />;
 
 export const BtGoodWhite = (style) => <Image
-  source={require('../material-design-icons/device/ios/ic_bluetooth_connected_white_48pt.imageset/ic_bluetooth_connected_white_48pt.png')}
+  source={require('../material-design-icons/ic_bluetooth_connected_white_48pt/ic_bluetooth_connected_white_48pt.png')}
   style={style}
 />;
 
 
 export const BtBadGrey = (style) => <Image
-  source={require('../material-design-icons/device/ios/ic_bluetooth_disabled_48pt.imageset/ic_bluetooth_disabled_48pt.png')}
+  source={require('../material-design-icons/ic_bluetooth_disabled_48pt/ic_bluetooth_disabled_48pt.png')}
   style={style}
 />;
 
 export const BtBadWhite = (style) => <Image
-  source={require('../material-design-icons/device/ios/ic_bluetooth_disabled_white_48pt.imageset/ic_bluetooth_disabled_white_48pt.png')}
+  source={require('../material-design-icons/ic_bluetooth_disabled_white_48pt/ic_bluetooth_disabled_white_48pt.png')}
   style={style}
 />;
 
